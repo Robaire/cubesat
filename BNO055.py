e09a35daea5a578919d50e8ffe5037cf65aea3cb
Robaire Galliath <robaire@users.noreply.github.com>
2019-11-17 19:47:28 -0500

    Add functions for reading sensor data

--- BNO055.py
+++ BNO055.py
@@ -4,7 +4,19 @@ import smbus
 ADDRESS = 0x28
 
 # Addresses of setting registers
+ST_RESULT = 0x36  # Contains the result of the startup self check
+OPR_MODE = 0x3d
+UNIT_SEL = 0x3b
 
+ACC_CONFIG = 0x08
+MAG_CONFIG = 0x09
+GYR_CONFIG = 0x0a
+
+# Addresses of data registers
+TEMP = 0x33
+ACC_DATA = 0x0d
+GYR_DATA = 0x19
+MAG_DATA = 0x13
 
 class BNO055:
     """ Setups and reads data from the BNO055 9 degree of freedom sensor. """
@@ -13,3 +25,43 @@ class BNO055:
 
         self.bus = smbus.SMBus(bus)
         self.address = address
+
+        # Set sensor settings
+        self.bus.write_byte_data(self.address, ACC_CONFIG, 0b00001100)  # 2g, 62.5Hz
+        self.bus.write_byte_data(self.address, MAG_CONFIG, 0b00011011)  # High Accuracy, 10Hz
+        self.bus.write_byte_data(self.address, GYR_CONFIG, 0b00001011)  # 250dps, 230Hz
+
+        # Set units for all sensors
+        self.bus.write_byte_data(self.address, UNIT_SEL, 0b10000000)  # Degrees, m/s^2, C
+
+        # Enter operating mode
+        self.bus.write_byte_data(self.address, OPR_MODE, 0b0111)
+
+    @staticmethod
+    def parse_axis(data):
+        """ Splits sensor data into component axis, combining MSB and LSB. """
+        z = (data[0] << 8) + data[1]
+        y = (data[2] << 8) + data[3]
+        x = (data[4] << 8) + data[5]
+        return z, y, x
+
+    def read_accel(self):
+        """ Read data from the accelerometer. """
+        data = self.bus.read_i2c_block_data(self.address, ACC_DATA, 6)
+
+        return self.parse_axis(data)
+
+    def read_gyro(self):
+        """ Read data from the gyroscope. """
+        data = self.bus.read_i2c_block_data(self.address, GYR_DATA, 6)
+        return self.parse_axis(data)
+
+    def read_mag(self):
+        """ Read data from the magnetometer. """
+        data = self.bus.read_i2c_block_data(self.address, MAG_DATA, 6)
+        return self.parse_axis(data)
+
+    def read_temp(self):
+        """ Read data from the temperature sensor. """
+        temp = self.bus.read_byte_data(self.address, TEMP)
+        return temp
