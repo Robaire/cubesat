ecb0ffa8628c76b20fa96aef0bf70f967bdbb551
Robaire Galliath <robaire@users.noreply.github.com>
2019-11-19 01:47:39 -0500

    String formatting

--- html/main.js
+++ html/main.js
@@ -2,9 +2,9 @@ let app = new Vue({
             el: '#app',
             data: {
                 sensor: {
-                    accel: [],
-                    gyro: [],
-                    mag: []
+                    accel: [0, 0, 0],
+                    gyro: [0, 0, 0],
+                    mag: [0, 0, 0]
                 },
                 pwm: 50
             },
@@ -24,6 +24,24 @@ let app = new Vue({
                 set_dutycycle: function() {
                     const message = JSON.stringify(this.pwm);
                     this.ws.send(message);
+                },
+
+                format_sensor_data: function(source) {
+                    let sensor;
+                    let digits;
+
+                    if(source == "accel"){
+                        sensor = this.sensor.accel;
+                        digits = 2;
+                    } else if(source == "gyro"){
+                        sensor = this.sensor.gyro;
+                        digits = 4;
+                    } else if(source == "mag"){
+                        sensor = this.sensor.mag;
+                        digits = 4;
+                    }
+
+                    return `[ ${sensor[0].toFixed(digits)}, ${sensor[1].toFixed(digits)}, ${sensor[2].toFixed(digits)} ]`
                 }
             }
         });
