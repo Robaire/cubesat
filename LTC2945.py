f193f527f13428c008f2fa61cc741f7e489b676f
Robaire Galliath <robaire@users.noreply.github.com>
2020-03-03 16:56:57 -0500

    Correct config error

--- LTC2945.py
+++ LTC2945.py
@@ -17,7 +17,7 @@ class LTC2945:
         self.address = address
 
         # Set sensor settings
-        self.bus.write_byte_data(self.address, CONTROL, 0b00000100)
+        self.bus.write_byte_data(self.address, CONTROL, 0b00000101)
         self.bus.write_byte_data(self.address, ALERT, 0b00000000)
 
     def read_voltage(self):
@@ -30,7 +30,7 @@ class LTC2945:
 
     def read(self):
         # print(self.bus.read_i2c_block_data(self.address, 0x00, 1))
-        # print(self.bus.read_i2c_block_data(self.address, 0x05, 3))
+        print(self.bus.read_i2c_block_data(self.address, 0x05, 3))
         # print(self.bus.read_i2c_block_data(self.address, 0x14, 2))
         # print(self.bus.read_i2c_block_data(self.address, 0x1E, 2))
         # print(self.bus.read_i2c_block_data(self.address, 0x28, 2))
