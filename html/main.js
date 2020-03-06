83e261953b43d81e89ceadc12c9f7bfee876c8a6
Robaire Galliath <robaire@users.noreply.github.com>
2020-03-05 20:47:55 -0500

    Create basic interface

--- html/main.js
+++ html/main.js
@@ -1,46 +1,68 @@
 let app = new Vue({
-            el: '#app',
-            data: {
-                logs: {
-                    acceleration: [],
-                    angularVelocity: [],
-                    magneticField: []
-                },
-                motor: {
-                    throttle: 0
-                },
-                test: {
-                    type: "example"
-                }
-            },
-            mounted: function() {
+    el: '#app',
+    data: {
+        voltage: 0,
+        current: 0,
+        target: {x: 0, y: 0, z: 0},
+        euler: {x: 0, y: 0, z: 0},
+        wheel: {x: 0, y: 0, z: 0}
+    },
 
-                // Create the Websocket
-                const loc = window.location;
-                const url = loc.protocol === "https:" ? "wss://" : "ws://";
-                this.ws = new WebSocket(url + loc.host + loc.pathname + "ws");
+    mounted: function() {
 
-                // Setup the callback to process received messages
-                this.ws.onmessage = (event) => {
-                    const message = JSON.parse(event.data);
+        // Create the Websocket
+        const loc = window.location;
+        const url = loc.protocol === "https:" ? "wss://" : "ws://";
+        this.ws = new WebSocket(url + loc.host + loc.pathname + "ws");
 
-                    if(message.type == "SENSOR_DATA"){
-                        this.logs.acceleration.push(message.acceleration);
-                        this.logs.angularVelocity.push(message.angularVelocity);
-                        this.logs.magneticField.push(message.magneticField);
-                    }
+        // Setup the callback to process received messages
+        this.ws.onmessage = (event) => {
+            const message = JSON.parse(event.data);
 
-                }
-            },
-            methods: {
-                sendMessage: function(header, body) {
-                    const message = JSON.stringify({header: header, body: body});
-                    this.ws.send(message);
-                },
-                setThrottle: function() {
-                    // Sends the motor throttle
-                    this.sendMessage("MOTOR_SPEED", this.motor);
-                }
-
+            if(message.type == "POWER_DATA"){
+                this.voltage = message.voltage;
+                this.current = message.current;
             }
-        });
+
+            if(message.type == "EULER_DATA"){
+                this.euler.x = message.x;
+                this.euler.y = message.y;
+                this.euler.z = message.z;
+            }
+
+            if(message.type == "WHEEL_DATA"){
+                this.wheel.x = message.x;
+                this.wheel.y = message.y;
+                this.wheel.z = message.z;
+            }
+
+            if(message.type == "TARGET_DATA"){
+                this.target.x = message.x;
+                this.target.y = message.y;
+                this.target.z = message.z;
+            }
+
+        }
+    },
+
+    methods: {
+        sendMessage: function(header, body) {
+            const message = JSON.stringify({header: header, body: body});
+            this.ws.send(message);
+        },
+        setThrottle: function() {
+            // Sends the motor throttle
+            this.sendMessage("MOTOR_SPEED", this.motor);
+        }
+
+    },
+
+    filters: {
+        short: function(number) {
+            return number.toFixed(2)
+        },
+        long: function(number) {
+            return number.toFixed(6)
+        }
+    }
+});
